7ce236b1c3099ab4c8cafe3ec4fd3aec2b8f6554
Giorgi Chibukhashvili <chibukhashviligiorgi@gmail.com>
2021-07-26 14:46:22 +0200

    completed 4.18

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const express = require('express')
 const app = express()
 const blogsRouter = require('./controllers/blogs')
 const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
+
 const cors = require('cors')
 const logger = require('./utils/logger')
 const mongoose = require('mongoose')
@@ -19,6 +21,8 @@ mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology
 
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
+app.use('/api/login', loginRouter)
+
 
   
 
